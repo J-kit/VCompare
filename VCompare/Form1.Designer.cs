83544a23d04028ab492f9e44566c2a313b60376a
J-kit <admin@j-kit.de>
2017-02-19 21:11:51 +0100

    Projektdateien hinzufügen.

---
+++ VCompare/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace VCompare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butSaveIndex = new System.Windows.Forms.Button();
+            this.butLoadIF = new System.Windows.Forms.Button();
+            this.butLoadFS = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.butCompare = new System.Windows.Forms.Button();
+            this.pbStatus = new System.Windows.Forms.ProgressBar();
+            this.lblProcStatus = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // butSaveIndex
+            // 
+            this.butSaveIndex.Location = new System.Drawing.Point(12, 103);
+            this.butSaveIndex.Name = "butSaveIndex";
+            this.butSaveIndex.Size = new System.Drawing.Size(113, 23);
+            this.butSaveIndex.TabIndex = 0;
+            this.butSaveIndex.Text = "Save IndexFile";
+            this.butSaveIndex.UseVisualStyleBackColor = true;
+            this.butSaveIndex.Click += new System.EventHandler(this.butSaveIndex_Click);
+            // 
+            // butLoadIF
+            // 
+            this.butLoadIF.Location = new System.Drawing.Point(6, 45);
+            this.butLoadIF.Name = "butLoadIF";
+            this.butLoadIF.Size = new System.Drawing.Size(107, 23);
+            this.butLoadIF.TabIndex = 1;
+            this.butLoadIF.Text = "Load Index File";
+            this.butLoadIF.UseVisualStyleBackColor = true;
+            this.butLoadIF.Click += new System.EventHandler(this.butLoadIF_Click);
+            // 
+            // butLoadFS
+            // 
+            this.butLoadFS.Location = new System.Drawing.Point(6, 16);
+            this.butLoadFS.Name = "butLoadFS";
+            this.butLoadFS.Size = new System.Drawing.Size(107, 23);
+            this.butLoadFS.TabIndex = 2;
+            this.butLoadFS.Text = "Load FileSystem";
+            this.butLoadFS.UseVisualStyleBackColor = true;
+            this.butLoadFS.Click += new System.EventHandler(this.butLoadFS_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.butLoadFS);
+            this.groupBox1.Controls.Add(this.butLoadIF);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(129, 74);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Load";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(147, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 74);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "FileSystem Info";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // butCompare
+            // 
+            this.butCompare.Location = new System.Drawing.Point(12, 132);
+            this.butCompare.Name = "butCompare";
+            this.butCompare.Size = new System.Drawing.Size(112, 41);
+            this.butCompare.TabIndex = 6;
+            this.butCompare.Text = "Compare with FileSystem";
+            this.butCompare.UseVisualStyleBackColor = true;
+            this.butCompare.Click += new System.EventHandler(this.butCompare_Click);
+            // 
+            // pbStatus
+            // 
+            this.pbStatus.Location = new System.Drawing.Point(156, 92);
+            this.pbStatus.Name = "pbStatus";
+            this.pbStatus.Size = new System.Drawing.Size(191, 23);
+            this.pbStatus.TabIndex = 1;
+            // 
+            // lblProcStatus
+            // 
+            this.lblProcStatus.AutoSize = true;
+            this.lblProcStatus.Location = new System.Drawing.Point(153, 118);
+            this.lblProcStatus.Name = "lblProcStatus";
+            this.lblProcStatus.Size = new System.Drawing.Size(35, 13);
+            this.lblProcStatus.TabIndex = 1;
+            this.lblProcStatus.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 199);
+            this.Controls.Add(this.lblProcStatus);
+            this.Controls.Add(this.pbStatus);
+            this.Controls.Add(this.butCompare);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.butSaveIndex);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butSaveIndex;
+        private System.Windows.Forms.Button butLoadIF;
+        private System.Windows.Forms.Button butLoadFS;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button butCompare;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar pbStatus;
+        private System.Windows.Forms.Label lblProcStatus;
+    }
+}
+
---
+++ VCompare/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VCompare;
+using Newtonsoft.Json;
+using VCompare.Utilities;
+
+using Microsoft.WindowsAPICodePack.Shell;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
+using System.Threading;
+
+namespace VCompare
+{
+    public partial class Form1 : Form
+    {
+        List<FInfo> LoadedFS
+        {
+            get
+            {
+                if (_LoadedFS == null)
+                    return new List<FInfo>();
+
+                return _LoadedFS;
+            }
+            set
+            {
+                _LoadedFS = value;
+                label1.Text = string.Format("Loaded Files: {0}\nTotal Size: {1}", _LoadedFS.Count, _LoadedFS.Sum(m => m.orisize).GetFormattedFileSize());
+            }
+        }
+        List<FInfo> _LoadedFS;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void butLoadFS_Click(object sender, EventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog
+            {
+                EnsurePathExists = true,
+                EnsureFileExists = false,
+                AllowNonFileSystemItems = false,
+                Title = "Select the directory that you want to Index.",
+                IsFolderPicker = true,
+            };
+
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                pbStatus.Style = ProgressBarStyle.Marquee;
+                lblProcStatus.Text = "FS Status: Initiated";
+                LoadFileSystem(dialog.FileName,
+                    new Action<List<FInfo>>((a) =>
+                    {
+                        LoadedFS = a;
+                        lblProcStatus.Text = "FS Status: Finished";
+                        pbStatus.Style = ProgressBarStyle.Blocks;
+                    }),
+                    new Action<string>(a => lblProcStatus.Text = a));
+            }
+        }
+
+
+        void LoadFileSystem(string path, Action<List<FInfo>> resAct = null, Action<string> UpdateStatus = null)
+        {
+            List<FInfo> pathlist = new List<FInfo>();
+            Thread th = new Thread(() =>
+            {
+                var FInfo = new DirectoryInfo(path).GetFiles("*", SearchOption.AllDirectories);
+                for (int i = 0; i < FInfo.Length; i++)
+                {
+                    var item = FInfo[i];
+                    if (UpdateStatus != null) this.BeginInvoke(UpdateStatus, string.Format("FS Status: Processing {0}/{1}", i, FInfo.Length));
+                    pathlist.Add(new FInfo()
+                    {
+                        FileName = item.Name,
+                        FilePath = item.FullName.Replace(path, ""),
+                        FileHash = item.ComputeHash(),
+                        orisize = item.Length
+                    });
+                }
+                if (resAct != null) this.Invoke(resAct, pathlist);
+            });
+            th.Start();
+        }
+
+        private void butLoadIF_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofdIndx = new OpenFileDialog();
+
+            ofdIndx.InitialDirectory = "c:\\";
+            ofdIndx.Filter = "INDX files (*.indx)|*.indx";
+            ofdIndx.FilterIndex = 0;
+            ofdIndx.RestoreDirectory = true;
+
+            if (ofdIndx.ShowDialog() == DialogResult.OK)
+            {
+                if (File.Exists(ofdIndx.FileName))
+                {
+                    try
+                    {
+                        LoadedFS = JsonConvert.DeserializeObject<List<FInfo>>(File.ReadAllText(ofdIndx.FileName));
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Something went wrong, idc what exactly, but fix it!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Retard mode ACTIVATED");
+                }
+            }
+        }
+
+        private void butSaveIndex_Click(object sender, EventArgs e)
+        {
+            if (LoadedFS != null)
+            {
+                SaveFileDialog sfdLoadedIndex = new SaveFileDialog();
+
+                sfdLoadedIndex.Filter = "INDX files (*.indx)|*.indx";
+                sfdLoadedIndex.FilterIndex = 2;
+                sfdLoadedIndex.RestoreDirectory = true;
+
+                if (sfdLoadedIndex.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(sfdLoadedIndex.FileName, JsonConvert.SerializeObject(LoadedFS));
+                }
+            }
+        }
+
+        private void butCompare_Click(object sender, EventArgs e)
+        {
+            List<string> neededCopy = new List<string>();
+            var cofdSelCompareDest = new CommonOpenFileDialog
+            {
+                EnsurePathExists = true,
+                EnsureFileExists = false,
+                AllowNonFileSystemItems = false,
+                DefaultFileName = "Select Folder",
+                Title = "Select the directory that you want to compare the vindex with.",
+                IsFolderPicker = true,
+            };
+
+            if (cofdSelCompareDest.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                var path = cofdSelCompareDest.FileName;
+                var FInfos = new DirectoryInfo(path).GetFiles("*", SearchOption.AllDirectories);
+
+                foreach (var FileInfo in FInfos)
+                {
+                    var relPath = FileInfo.FullName.Replace(path, ""); //\unins000.exe"
+                    var comEntity = LoadedFS.Where(m => m.FilePath == relPath).FirstOrDefault();
+
+                    if (comEntity == null || comEntity.orisize != FileInfo.Length || FileInfo.ComputeHash() != comEntity.FileHash)
+                        neededCopy.Add(relPath);
+                }
+
+                if (neededCopy.Count == 0)
+                {
+                    MessageBox.Show("Didn't find any differences, everything should be fine!");
+                }
+                else
+                {
+                    var userSelDifferDirCreation = MessageBox.Show(string.Format("Found {0} files that differ from the vindex file, do you want to create a difference directory?", neededCopy.Count), "Changes Found", MessageBoxButtons.YesNo) == DialogResult.Yes ? true : false;
+                    if (userSelDifferDirCreation)
+                    {
+                        var cofdMoveToFolder = new CommonOpenFileDialog
+                        {
+                            EnsurePathExists = true,
+                            EnsureFileExists = false,
+                            AllowNonFileSystemItems = false,
+                            DefaultFileName = "Select Folder",
+                            Title = "Select the directory that you want to move the different files in.",
+                            IsFolderPicker = true,
+                        };
+
+                        if (cofdMoveToFolder.ShowDialog() == CommonFileDialogResult.Ok)
+                        {
+                            var cTPath = cofdMoveToFolder.FileName;
+                            foreach (var copItem in neededCopy)
+                            {
+                                Directory.CreateDirectory(new FileInfo(cTPath + copItem).DirectoryName);
+                                File.Copy(path + copItem, cTPath + copItem);
+                            }
+                            MessageBox.Show(string.Format("Finished copying {0} files", neededCopy.Count));
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+    }
+
+    public class FInfo
+    {
+        public string FilePath;
+        public string FileName;
+        public string FileHash;
+        public long orisize;
+    }
+
+    public static class Extentions
+    {
+        public static string ComputeHash(this FileInfo input, bool lowercase = true)
+        {
+            if (!input.Exists)
+                throw new Exception("The requested file was not found!");
+
+            using (Stream fs = input.OpenRead())
+                return MD5.Create().ComputeHash(fs).ToHexString(lowercase);
+        }
+
+
+        static string[] sizeSuffixes = {
+        "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+
+        public static string GetFormattedFileSize(this long size)
+        {
+            Debug.Assert(sizeSuffixes.Length > 0);
+
+            const string formatTemplate = "{0}{1:0.#} {2}";
+
+            if (size == 0)
+            {
+                return string.Format(formatTemplate, null, 0, sizeSuffixes[0]);
+            }
+
+            var absSize = Math.Abs((double)size);
+            var fpPower = Math.Log(absSize, 1000);
+            var intPower = (int)fpPower;
+            var iUnit = intPower >= sizeSuffixes.Length
+                ? sizeSuffixes.Length - 1
+                : intPower;
+            var normSize = absSize / Math.Pow(1000, iUnit);
+
+            return string.Format(
+                formatTemplate,
+                size < 0 ? "-" : null, normSize, sizeSuffixes[iUnit]);
+        }
+
+
+
+        public static bool Exists(this FileInfo input) => File.Exists(input.FullName);
+        public static Stream OpenRead(this FileInfo input) => File.OpenRead(input.FullName);
+        public static string ToHexString(this byte[] input, bool lowercase = true) => string.Concat(input.Select(b => b.ToString(lowercase ? "x2" : "X2")).ToArray());
+    }
+}
